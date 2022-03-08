b7fd1b8e069a1bcf2070b1314b4cd7295d42997d
Sebastian Sixt <l1322@schueler.ms-muc.de>
2022-03-08 10:08:32 +0100

    R-TRIG eingefügt

--- TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
@@ -55,6 +55,7 @@ extern "C" void app_main(void)
     TON1.PT = 1000;
 
     RS RS1;
+    R_TRIG R_TRIG1;
 
 
     while (true) // Endlos-Schleife
@@ -65,9 +66,11 @@ extern "C" void app_main(void)
 
         // den I1 an TON1 uebergeben, und TON1 aufrufen
         TON1(I1);
+        R_TRIG1(I1);
 
 
-        RS1(TON1.Q,I2);
+
+        RS1(TON1.Q,R_TRIG1.Q);
 
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, RS1.Q1);
